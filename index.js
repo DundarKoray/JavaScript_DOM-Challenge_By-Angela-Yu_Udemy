9354d6dfc567cbe9817c69b80d084126a94b8518
Koray Dundar <koraydundar_40@hotmail.com>
2019-03-31 12:54:58 +0300

    Created random numbers for dices and change their images according to the number

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 //create a dice
-function dice () {
-  let dice = Math.floor( Math.random()*6 +1)
-  console.log(dice)
-}
 
-dice()
+  let dice1 = Math.floor( (Math.random() * 6) + 1 )
+  let dice2 = Math.floor( (Math.random() * 6) + 1 )
+  
+  let diceImg1 = `dice${dice1}.png`
+  let diceSource1 = `images/${diceImg1}`
+  
+  let diceImg2 = `dice${dice2}.png`
+  let diceSource2 = `images/${diceImg2}`
+
+  document.querySelector('.img1').setAttribute('src', diceSource1)
+  document.querySelector('.img2').setAttribute('src', diceSource2)
+  
